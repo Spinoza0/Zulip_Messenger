4150182438bf84c04454db888b542f75b39c5854
Spinoza0 <58294846+Spinoza0@users.noreply.github.com>
2023-02-25 08:29:47 +0300

    refactoring (add GetContactsResult)

--- HomeWork_1-4_5/src/test/java/com/spinoza/homework_1/domain/ContactTest.kt
+++ HomeWork_1-4_5/src/test/java/com/spinoza/homework_1/domain/ContactTest.kt
@@ -1,16 +1,13 @@
 package com.spinoza.homework_1.domain
 
 import org.junit.Assert.assertEquals
-import org.junit.Assert.assertNotEquals
 import org.junit.Test
 
 class ContactTest {
     @Test
     fun contactsEqualTest() {
         val contact1 = Contact("John", "123")
-        val contact2 = Contact("Mary", "456")
-        val contact3 = Contact("John", "123")
-        assertEquals(contact1, contact3)
-        assertNotEquals(contact1, contact2)
+        val contact2 = Contact("John", "123")
+        assertEquals(contact1, contact2)
     }
 }
