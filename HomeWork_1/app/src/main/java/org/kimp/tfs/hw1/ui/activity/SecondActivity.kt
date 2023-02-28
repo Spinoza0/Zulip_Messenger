c270f70b8b05c7d2e4a61a2988dc65b795cff2c0
KonstantIMP <konstantimp@yandex.ru>
2023-02-28 06:58:11 +0300

    feat: declare the second activity

---
+++ HomeWork_1/app/src/main/java/org/kimp/tfs/hw1/ui/activity/SecondActivity.kt
@@ -0,0 +1,12 @@
+package org.kimp.tfs.hw1.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SecondActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
