c8130d59c27447e30b79a951c7af910e66ef62f5
KonstantIMP <konstantimp@yandex.ru>
2023-02-28 07:02:53 +0300

    feat: declare layouts (empty)

--- HomeWork_1/app/src/main/java/org/kimp/tfs/hw1/ui/activity/FirstActivity.kt
+++ HomeWork_1/app/src/main/java/org/kimp/tfs/hw1/ui/activity/FirstActivity.kt
@@ -3,10 +3,16 @@ package org.kimp.tfs.hw1.ui.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
+import org.kimp.tfs.hw1.databinding.ActivityFirstLayoutBinding
 
 @AndroidEntryPoint
 class FirstActivity: AppCompatActivity() {
+    private lateinit var binding: ActivityFirstLayoutBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = ActivityFirstLayoutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
--- HomeWork_1/app/src/main/java/org/kimp/tfs/hw1/ui/activity/SecondActivity.kt
+++ HomeWork_1/app/src/main/java/org/kimp/tfs/hw1/ui/activity/SecondActivity.kt
@@ -3,10 +3,16 @@ package org.kimp.tfs.hw1.ui.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
+import org.kimp.tfs.hw1.databinding.ActivitySecondLayoutBinding
 
 @AndroidEntryPoint
 class SecondActivity: AppCompatActivity() {
+    private lateinit var binding: ActivitySecondLayoutBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = ActivitySecondLayoutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
