64e951cea4607a5ebd6b7da94ae0c8330850281a
KonstantIMP <konstantimp@yandex.ru>
2023-02-28 07:14:34 +0300

    feat: call the second activity for result by request data button click

--- HomeWork_1/app/src/main/java/org/kimp/tfs/hw1/ui/activity/FirstActivity.kt
+++ HomeWork_1/app/src/main/java/org/kimp/tfs/hw1/ui/activity/FirstActivity.kt
@@ -1,18 +1,43 @@
 package org.kimp.tfs.hw1.ui.activity
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
+import org.kimp.tfs.hw1.HomeworkApplication
 import org.kimp.tfs.hw1.databinding.ActivityFirstLayoutBinding
+import timber.log.Timber
 
 @AndroidEntryPoint
 class FirstActivity: AppCompatActivity() {
     private lateinit var binding: ActivityFirstLayoutBinding
+
+    private val secondActivityResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        if (it.resultCode == Activity.RESULT_OK) {
+            Timber.tag(HomeworkApplication.TAG)
+                .i("Received OK result from the SecondActivity")
+        } else {
+            Timber.tag(HomeworkApplication.TAG)
+                .i("SecondActivity didn't return OK result")
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityFirstLayoutBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        connectActions()
+    }
+
+    private fun connectActions() {
+        binding.requestDataButton.setOnClickListener {
+            secondActivityResult.launch(
+                Intent(this, SecondActivity::class.java)
+            )
+        }
     }
 }
