168bf042fde2dfa53840a9da21e7315d87b945aa
Spinoza0 <58294846+Spinoza0@users.noreply.github.com>
2023-03-09 15:56:46 +0300

    add setMessageActionImage

--- messenger-tfs/src/main/java/com/spinoza/messenger_tfs/presentation/fragment/MessagesFragment.kt
+++ messenger-tfs/src/main/java/com/spinoza/messenger_tfs/presentation/fragment/MessagesFragment.kt
@@ -1,11 +1,14 @@
 package com.spinoza.messenger_tfs.presentation.fragment
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.spinoza.messenger_tfs.R
 import com.spinoza.messenger_tfs.databinding.FragmentMessagesBinding
 
 class MessagesFragment : Fragment() {
@@ -26,12 +29,39 @@ class MessagesFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setupScreen()
+    }
+
+    private fun setupScreen() {
         binding.toolbar.setNavigationOnClickListener {
             if (!findNavController().popBackStack()) {
                 requireActivity().finish()
             }
         }
+
+        binding.editTextMessage.addTextChangedListener(object : TextWatcher {
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                setMessageActionImage()
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun afterTextChanged(s: Editable?) {}
+        })
     }
+
+    private fun setMessageActionImage() {
+        if (isMessageEmpty()) {
+            binding.imageViewAction.setImageResource(R.drawable.ic_add_circle_outline)
+        } else {
+            binding.imageViewAction.setImageResource(R.drawable.ic_send)
+        }
+    }
+
+    private fun isMessageEmpty(): Boolean {
+        return binding.editTextMessage.text.toString().trim().isEmpty()
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
