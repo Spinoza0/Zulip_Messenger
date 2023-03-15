185f7bd32f0dfae5859e266ef85fb0874beeafa0
Spinoza0 <58294846+Spinoza0@users.noreply.github.com>
2023-03-15 13:34:08 +0300

    add bottom navigation resources

---
+++ messenger-tfs/src/main/java/com/spinoza/messenger_tfs/presentation/fragment/MainFragment.kt
@@ -0,0 +1,40 @@
+package com.spinoza.messenger_tfs.presentation.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.spinoza.messenger_tfs.databinding.FragmentMainBinding
+
+
+class MainFragment : Fragment() {
+
+    private var _binding: FragmentMainBinding? = null
+    private val binding: FragmentMainBinding
+        get() = _binding ?: throw RuntimeException("FragmentMainBinding == null")
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        _binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object {
+        fun newInstance(): MainFragment {
+            return MainFragment()
+        }
+    }
+}
