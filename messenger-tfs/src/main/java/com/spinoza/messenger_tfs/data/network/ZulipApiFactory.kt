aae3662cf96cd0a138062c2fcb2c32e741ec2e64
Spinoza0 <58294846+Spinoza0@users.noreply.github.com>
2023-04-03 12:26:49 +0300

    restore base_url

--- messenger-tfs/src/main/java/com/spinoza/messenger_tfs/data/network/ZulipApiFactory.kt
+++ messenger-tfs/src/main/java/com/spinoza/messenger_tfs/data/network/ZulipApiFactory.kt
@@ -12,13 +12,9 @@ object ZulipApiFactory {
     private const val HEADER_AUTHORIZATION = "Authorization"
     private const val MEDIA_TYPE_JSON = "application/json"
 
-    private const val BASE_URL = "https://victoryvalery.zulipchat.com/api/v1/"
-    private const val CREDENTIALS_USERNAME = "ivan.sintyurin@gmail.com"
-    private const val CREDENTIALS_PASSWORD = "RaINyfjtFHz8KEUFXtXzxPcVVRjaDdrm"
-
-    // private const val BASE_URL = "https://tinkoff-android-spring-2023.zulipchat.com/api/v1/"
-    // private const val CREDENTIALS_USERNAME = "spinoza0@gmail.com"
-    // private const val CREDENTIALS_PASSWORD = "Tu1s51Gtq1ec02fBd1lhAaOALD0hc2JH"
+     private const val BASE_URL = "https://tinkoff-android-spring-2023.zulipchat.com/api/v1/"
+     private const val CREDENTIALS_USERNAME = "spinoza0@gmail.com"
+     private const val CREDENTIALS_PASSWORD = "Tu1s51Gtq1ec02fBd1lhAaOALD0hc2JH"
 
     private const val TIME_OUT_SECONDS = 15L
     private val json = Json {
