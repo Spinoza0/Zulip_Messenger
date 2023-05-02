638c810f7ff9cc9a6ea28a3febae3c342d698b30
Spinoza0 <58294846+Spinoza0@users.noreply.github.com>
2023-05-03 01:04:06 +0300

    add tests: messageWithReactionsIsVisible, messageWithoutReactionsIsVisible

--- messenger-tfs/src/androidTest/java/com/spinoza/messenger_tfs/screen/MessagesScreen.kt
+++ messenger-tfs/src/androidTest/java/com/spinoza/messenger_tfs/screen/MessagesScreen.kt
@@ -5,6 +5,7 @@ import com.kaspersky.kaspresso.screens.KScreen
 import com.spinoza.messenger_tfs.R
 import com.spinoza.messenger_tfs.presentation.feature.messages.MessagesFragment
 import io.github.kakaocup.kakao.common.views.KView
+import io.github.kakaocup.kakao.image.KImageView
 import io.github.kakaocup.kakao.recycler.KRecyclerItem
 import io.github.kakaocup.kakao.recycler.KRecyclerView
 import io.github.kakaocup.kakao.text.KTextView
@@ -21,8 +22,6 @@ class MessagesScreen : KScreen<MessagesScreen>() {
     val chooseReactionDialogTopLine = KView { withId(R.id.textViewTopLine) }
 
     class MessageItem(parent: Matcher<View>) : KRecyclerItem<MessageItem>(parent) {
-        val name = KTextView(parent) { withId(R.id.nameTextView) }
-        val content = KTextView(parent) { withId(R.id.contentTextView) }
-        val reactions = KView(parent) { withId(R.id.reactionsFlexBoxLayout) }
+        val iconAddReaction = KImageView(parent) { withId(R.id.flexbox_icon_add) }
     }
 }
