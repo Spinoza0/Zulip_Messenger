e062e322ce0904f0e200c94e60990395efd971f7
Spinoza0 <58294846+Spinoza0@users.noreply.github.com>
2023-05-02 15:01:36 +0300

    add MainMenuTest.peopleScreen_IsVisible

--- messenger-tfs/src/androidTest/java/com/spinoza/messenger_tfs/MainMenuTest.kt
+++ messenger-tfs/src/androidTest/java/com/spinoza/messenger_tfs/MainMenuTest.kt
@@ -3,7 +3,9 @@ package com.spinoza.messenger_tfs
 import androidx.test.ext.junit.rules.activityScenarioRule
 import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
 import com.spinoza.messenger_tfs.presentation.feature.app.MainActivity
+import com.spinoza.messenger_tfs.screen.ChannelsScreen
 import com.spinoza.messenger_tfs.screen.MainMenuScreen
+import com.spinoza.messenger_tfs.screen.PeopleScreen
 import org.junit.Rule
 import org.junit.Test
 
@@ -13,10 +15,28 @@ class MainMenuTest : TestCase() {
     val activityRule = activityScenarioRule<MainActivity>()
 
     @Test
-    fun bottomNavigationMenu_IsVisible() = run {
+    fun channelsScreenIsVisible_ByDefault() = run {
         val mainMenuScreen = MainMenuScreen()
+        val channelsScreen = ChannelsScreen()
         step("Bottom navigation menu is visible") {
             mainMenuScreen.bottomMenu.isVisible()
+        }
+        step("Channels screen is visible") {
+            channelsScreen.searchField.isVisible()
+            channelsScreen.tabLayout.isVisible()
+        }
+    }
+
+    @Test
+    fun peopleScreen_IsVisible() = run {
+        val mainMenuScreen = MainMenuScreen()
+        val peopleScreen = PeopleScreen()
+        step("Open the people screen") {
+            mainMenuScreen.bottomMenu.click()
+        }
+        step("Users list is visible") {
+            peopleScreen.searchField.isVisible()
+            peopleScreen.usersList.isVisible()
         }
     }
 }
---
+++ messenger-tfs/src/androidTest/java/com/spinoza/messenger_tfs/screen/PeopleScreen.kt
@@ -0,0 +1,26 @@
+package com.spinoza.messenger_tfs.screen
+
+import android.view.View
+import com.kaspersky.kaspresso.screens.KScreen
+import com.spinoza.messenger_tfs.R
+import com.spinoza.messenger_tfs.presentation.feature.people.PeopleFragment
+import io.github.kakaocup.kakao.edit.KEditText
+import io.github.kakaocup.kakao.recycler.KRecyclerItem
+import io.github.kakaocup.kakao.recycler.KRecyclerView
+import io.github.kakaocup.kakao.text.KTextView
+import org.hamcrest.Matcher
+
+class PeopleScreen : KScreen<PeopleScreen>() {
+
+    override val layoutId: Int = R.layout.fragment_people
+    override val viewClass: Class<*> = PeopleFragment::class.java
+
+    val searchField = KEditText { withId(R.id.editTextSearch) }
+    val usersList =
+        KRecyclerView({ withId(R.id.recyclerViewUsers) }, { itemType { UserItem(it) } })
+
+    class UserItem(parent: Matcher<View>) : KRecyclerItem<UserItem>(parent) {
+        val name = KTextView(parent) { withId(R.id.textViewName) }
+        val email = KTextView(parent) { withId(R.id.textViewEmail) }
+    }
+}
