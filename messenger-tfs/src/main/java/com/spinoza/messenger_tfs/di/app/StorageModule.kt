b96f2d8254b3a3b6cca9e82b56b23bc4c3dedd77
Spinoza0 <58294846+Spinoza0@users.noreply.github.com>
2023-05-05 14:20:25 +0300

    fix: work of UI-tests without real authorization

--- messenger-tfs/src/main/java/com/spinoza/messenger_tfs/di/app/StorageModule.kt
+++ messenger-tfs/src/main/java/com/spinoza/messenger_tfs/di/app/StorageModule.kt
@@ -5,30 +5,40 @@ import androidx.room.Room
 import com.spinoza.messenger_tfs.BuildConfig
 import com.spinoza.messenger_tfs.data.database.MessengerDao
 import com.spinoza.messenger_tfs.data.database.MessengerDatabase
+import com.spinoza.messenger_tfs.data.usermanager.UserManagerImpl
 import com.spinoza.messenger_tfs.di.ApplicationScope
 import com.spinoza.messenger_tfs.di.BaseUrl
+import com.spinoza.messenger_tfs.domain.usermanager.UserManager
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 
 @Module
-object StorageModule {
+interface StorageModule {
 
     @ApplicationScope
-    @Provides
-    @BaseUrl
-    fun provideBaseUrl(): String = if (BuildConfig.ZULIP_SERVER_URL.endsWith(SLASH)) {
-        BuildConfig.ZULIP_SERVER_URL.dropLast(SLASH.length)
-    } else {
-        BuildConfig.ZULIP_SERVER_URL
+    @Binds
+    fun bindUserManager(impl: UserManagerImpl): UserManager
+
+    companion object {
+
+        @ApplicationScope
+        @Provides
+        @BaseUrl
+        fun provideBaseUrl(): String = if (BuildConfig.ZULIP_SERVER_URL.endsWith(SLASH)) {
+            BuildConfig.ZULIP_SERVER_URL.dropLast(SLASH.length)
+        } else {
+            BuildConfig.ZULIP_SERVER_URL
+        }
+
+        @ApplicationScope
+        @Provides
+        fun provideMessengerDao(context: Context): MessengerDao =
+            Room.databaseBuilder(context, MessengerDatabase::class.java, BuildConfig.DATABASE_NAME)
+                .fallbackToDestructiveMigration()
+                .build()
+                .dao()
+
+        private const val SLASH = "/"
     }
-
-    @ApplicationScope
-    @Provides
-    fun provideMessengerDao(context: Context): MessengerDao =
-        Room.databaseBuilder(context, MessengerDatabase::class.java, BuildConfig.DATABASE_NAME)
-            .fallbackToDestructiveMigration()
-            .build()
-            .dao()
-
-    private const val SLASH = "/"
 }
